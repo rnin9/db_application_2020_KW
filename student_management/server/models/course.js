00b5f5549ab15e8096e5e9b8826ad3834e34e19f
meltnia <72488905+meltnia@users.noreply.github.com>
2020-11-21 18:26:35 +0900

    Update course.js

--- student_management/server/models/course.js
+++ student_management/server/models/course.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) =>{
-   return sequelize.define('course',{   // Course table
+   return sequelize.define('COURSE',{   // Course table
       Course_num:{
-         type: DataTypes.char(11),// 아직 test 데이터베이스에선 varchar(15)로 되어있으니 수정 필요.
+         type: DataTypes.STRING(15),// -도 포함한다면 14정도의 길이가 됨
          allowNull: false,
          primaryKey: true,
       },
@@ -14,48 +14,48 @@ module.exports = (sequelize, DataTypes) =>{
          primaryKey: true,
       },
       department:{
-         type: DataTypes.char(12)
+         type: DataTypes.STRING(12)
       },
       classification:{
          type: DataTypes.INTEGER//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
       },
       Course_name:{
-         type: DataTypes.varchar(20)
+         type: DataTypes.STRING(20)
       },
       headcount_now:{
-         type: DataTypes.INTEGER.unsigned//SMALLINT가 없어서 INTEGER로 대체
+         type: DataTypes.INTEGER.UNSIGNED//SMALLINT가 없어서 INTEGER로 대체
       },
       headcount_maximum:{
-         type: DataTypes.INTEGER.unsigned//SMALLINT가 없어서 INTEGER로 대체
+         type: DataTypes.INTEGER.UNSIGNED//SMALLINT가 없어서 INTEGER로 대체
       },
       ct_mon:{//class time monday
-         type: DataTypes.INTEGER.unsigned,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
+         type: DataTypes.INTEGER.UNSIGNED,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
 		 defaultValue : 0
       },
       ct_tue:{
-         type: DataTypes.INTEGER.unsigned,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
+         type: DataTypes.INTEGER.UNSIGNED,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
 		 defaultValue : 0
       },
       ct_wed:{
-         type: DataTypes.INTEGER.unsigned,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
+         type: DataTypes.INTEGER.UNSIGNED,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
 		 defaultValue : 0
       },
       ct_thr:{
-         type: DataTypes.INTEGER.unsigned,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
+         type: DataTypes.INTEGER.UNSIGNED,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
 		 defaultValue : 0
       },
       ct_fri:{
-         type: DataTypes.INTEGER.unsigned,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
+         type: DataTypes.INTEGER.UNSIGNED,//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
 		 defaultValue : 0
       },
       credit:{
-         type: DataTypes.INTEGER.unsigned//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
+         type: DataTypes.INTEGER.UNSIGNED//TINYINT가 존재하지 않아서 일단 INTEGER로 대체.
       },
       room:{
-         type: DataTypes.varchar(20)
+         type: DataTypes.STRING(20)
       },
       professor_id:{
-         type: DataTypes.varchar(30)
+         type: DataTypes.STRING(30)
       },
    },{ timestamps: false});
 }
