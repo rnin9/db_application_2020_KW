939f1f16659ef759ca48fe241dbe9241473dd779
rnin9 <rkdalswn1209@kw.ac.kr>
2020-12-02 11:14:29 +0900

    Filter on CourseReg

--- student_management/src/components/views/CourseRegPage/CourseRegPage.js
+++ student_management/src/components/views/CourseRegPage/CourseRegPage.js
@@ -1,81 +1,15 @@
 import React, { Component } from 'react';
-import { AutoComplete, Table, Form, Button, message } from 'antd';
+import { AutoComplete, Table, Form, Button, message, Input, Space } from 'antd';
 import './CourseRegPage.css'
 import axios from 'axios';
-import { parseTwoDigitYear } from 'moment';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
+
 
 const gyear = localStorage.getItem('year');
 const gsem = localStorage.getItem('semester');
 
-const columns = [
-  {
-    title: '학정번호',
-    dataIndex: 'Course_num',
-    key: 'Course_num',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.userGrade - b.userGrade,
-  },
-  {
-    title: '과목명',
-    dataIndex: 'Course_name',
-    key: 'Course_name',
-  },
-  
-  {
-    title: '학과',
-    dataIndex: 'major',
-    key: 'major',
-    filters: [
-      {
-        text: '컴정공',
-        value: '컴퓨터정보공학',
-      },
-      {
-        text: '컴소',
-        value: '컴퓨터소프트웨어',
-      },
-      {
-        text: '전기공',
-        value: '전기공학',
-      },
-    ],
-    filterMultiple: false,
-    onFilter: (value, record) => record.major.indexOf(value) !== -1
-  },
-  {
-    title: '이수',
-    dataIndex: 'classification',
-    key: 'classification',
-    filters: [
-      {
-        text: '교양',
-        value: '교',
-      },
-      {
-        text: '전공',
-        value: '전',
-      },
-      {
-        text: '기초',
-        value: '기',
-      },
-  ],
-    filterMultiple: true,
-    onFilter: (value, record) => record.classification.indexOf(value) !== -1
-  },
-  {
-    title: '학점',
-    dataIndex: 'credit',
-    key: 'credit',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.credit - b.credit,
-  },
-  {
-    title: '강의시간',
-    dataIndex: 'class_time',
-    key: 'class_time',
-  },
-];
+
 
 const rowSelection = {
   onChange:(selectedRowKeys, selectedRows, e) => {
@@ -83,7 +17,6 @@ const rowSelection = {
     console.log(e.currentTarget.value)
   }
 }
-
 const id = localStorage.getItem('id')
 
 class CourseRegPage extends Component{
@@ -96,12 +29,81 @@ class CourseRegPage extends Component{
           list : [],
           row :[],
           update : false,
+          searchText: '',
+          searchedColumn: '',
         }    
       }
     
     componentDidMount(){
       this._getData()
     }
+
+    getColumnSearchProps = dataIndex => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 8 }}>
+          <Input
+            ref={node => {
+              this.searchInput = node;
+            }}
+            placeholder={'강의찾기'}
+            value={selectedKeys[0]}
+            onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{ width: 188, marginBottom: 8, display: 'block' }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{ width: 90 }}
+            >
+              Search
+            </Button>
+            <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+              Reset
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+      onFilter: (value, record) =>
+        record[dataIndex]
+          ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
+          : '',
+      onFilterDropdownVisibleChange: visible => {
+        if (visible) {
+          setTimeout(() => this.searchInput.select(), 100);
+        }
+      },
+      render: text =>
+        this.state.searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+            searchWords={[this.state.searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+          />
+        ) : (
+          text
+        ),
+    });
+
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+      confirm();
+      this.setState({
+        searchText: selectedKeys[0],
+        searchedColumn: dataIndex,
+      });
+    };
+  
+    handleReset = clearFilters => {
+      clearFilters();
+      this.setState({ searchText: '' });
+    };
+  
+
 
     _getData = async () => {
       const res = await axios.get('/api/course', {params : {year : gyear, sem: gsem}});     
@@ -141,6 +143,76 @@ class CourseRegPage extends Component{
 
     render(){
         const { list } = this.state;
+        const columns = [
+          {
+            title: '학정번호',
+            dataIndex: 'Course_num',
+            key: 'Course_num',
+            defaultSortOrder: 'descend',
+            sorter: (a, b) => a.userGrade - b.userGrade,
+          },
+          {
+            title: '과목명',
+            dataIndex: 'Course_name',
+            key: 'Course_name',
+            ...this.getColumnSearchProps('Course_name'),
+          },
+          
+          {
+            title: '학과',
+            dataIndex: 'major',
+            key: 'major',
+            filters: [
+              {
+                text: '컴정공',
+                value: '컴퓨터정보공학',
+              },
+              {
+                text: '컴소',
+                value: '컴퓨터소프트웨어',
+              },
+              {
+                text: '전기공',
+                value: '전기공학',
+              },
+            ],
+            filterMultiple: false,
+            onFilter: (value, record) => record.major.indexOf(value) !== -1
+          },
+          {
+            title: '이수',
+            dataIndex: 'classification',
+            key: 'classification',
+            filters: [
+              {
+                text: '교양',
+                value: '교',
+              },
+              {
+                text: '전공',
+                value: '전',
+              },
+              {
+                text: '기초',
+                value: '기',
+              },
+          ],
+            filterMultiple: true,
+            onFilter: (value, record) => record.classification.indexOf(value) !== -1
+          },
+          {
+            title: '학점',
+            dataIndex: 'credit',
+            key: 'credit',
+            defaultSortOrder: 'descend',
+            sorter: (a, b) => a.credit - b.credit,
+          },
+          {
+            title: '강의시간',
+            dataIndex: 'class_time',
+            key: 'class_time',
+          },
+        ];
         
         return(
           <div style={{margin: AutoComplete}}> 
