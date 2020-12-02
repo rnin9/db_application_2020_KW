bbdff81fa8713738855ac3abbf63d4e0e4a0411d
rnin9 <rkdalswn1209@kw.ac.kr>
2020-12-02 10:58:27 +0900

    LandingPage Design

--- student_management/src/components/views/LandingPage/LandingPage.js
+++ student_management/src/components/views/LandingPage/LandingPage.js
@@ -1,88 +1,79 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux'
-import { AutoComplete, Carousel } from 'antd';
+import { AutoComplete, Carousel, Card, Modal} from 'antd';
 import './LandingPage.css'
 import axios from 'axios';
 import { requestReceived } from '../../../redux/_actions/friend_actions';
 import p1 from './LandingImage/1.jpg'
 import p2 from './LandingImage/2.jpg'
 import p3 from './LandingImage/3.jpg'
+import { GithubFilled, TeamOutlined, YoutubeFilled } from '@ant-design/icons';
 
 
-const columns = [
-  {
-    title: 'ID',
-    dataIndex: 'userID',
-    key: 'id',
-  },
-  {
-    title: 'Name',
-    dataIndex: 'userName',
-    key: 'name',
-  },
+function LandingPage (){
 
-  {
-    title: 'Password',
-    dataIndex: 'userPassword',
-    key: 'pw',
-  },
-  {
-    title: '이메일',
-    dataIndex: 'userEmail',
-    key: 'email',
-  },
-  {
-    title: '주소',
-    dataIndex: 'userAddress',
-    key: 'add',
-  },
-];
-const id = localStorage.getItem('id')
-function LandingPage(props) {
-  const [list, setlist] = useState([])
-  const dispatch = useDispatch()
+  const gridStyle = {
+    width: '25%',
+    textAlign: 'center',
+  };
 
-  useEffect(() => {
-    _getData()
-    dispatch(requestReceived(id))
-  }, [])
-
-  const _getData = async () => {
-    const res = await axios.get('/api/user');
-    if (res.data[0] === undefined) {
-      let cover = [];
-      cover.push(res.data);       // response 데이터들 push
-      setlist(cover)
-    } else
-      setlist(res.data);
+  const handleClick = ()=>{
+    Modal.info({
+      title: 'Team : 143',
+      content: (
+        <div>
+          <p>2014722090 강민주 (팀장)</p>
+          <p>2014722092 오영헌</p>
+          <p>2014722034 이영주</p>
+        </div>
+      ),
+      onOk() {},
+    });
+  
   }
 
-  return (
-    <div style={{ margin: AutoComplete }}>
-      <Carousel autoplay style={{display:"flex", justifyContent:"center"}}>
-        <div className="land_img">
-        <a href="https://www.kw.ac.kr/ko/life/notice.jsp?BoardMode=view&DUID=34156&tpage=1&searchKey=1&searchVal=%EA%B3%B5%EA%B0%90&srCategoryId="  target="_blank" rel="noopener noreferrer"
+return (
+  <div style={{ margin: AutoComplete }} className="head">
+    <h2 style={{paddingTop:30, paddingBottom:20}}> 학생 관리 시스템 </h2>
+    <Carousel autoplay style={{ display: "flex", justifyContent: "center" }}>
+      <div className="land_img">
+        <a href="https://www.kw.ac.kr/ko/life/notice.jsp?BoardMode=view&DUID=34156&tpage=1&searchKey=1&searchVal=%EA%B3%B5%EA%B0%90&srCategoryId=" target="_blank" rel="noopener noreferrer"
         >
-          <img src={p1} style={{width: '100%',display:"flex", justifyContent:"center",paddingRight:70, paddingLeft:70}}></img>
+          <img src={p1} style={{ width: '100%', display: "flex", justifyContent: "center", paddingRight: 70, paddingLeft: 70 }}></img>
         </a>
-        </div>
-        <div className="land_img">
-        <a href="https://www.kw.ac.kr/ko/life/newsletter.jsp?BoardMode=view&DUID=33812"  target="_blank" rel="noopener noreferrer" >
-        <img src={p2} style={{width: '100%',display:"flex", justifyContent:"center",paddingRight:70, paddingLeft:70}}></img>
-        </a>
-       </div>
-        <div className="land_img">
-        <a href="https://www.kw.ac.kr/ko/life/corona.jsp"  target="_blank" rel="noopener noreferrer" >
-        <img src={p3} style={{width: '100%',display:"flex", justifyContent:"center",paddingRight:70, paddingLeft:70}}></img>
-        </a>
-        </div>
-        </Carousel>
-      <div className="table_user">
       </div>
-    </div>
+      <div className="land_img">
+        <a href="https://www.kw.ac.kr/ko/life/newsletter.jsp?BoardMode=view&DUID=33812" target="_blank" rel="noopener noreferrer" >
+          <img src={p2} style={{ width: '100%', display: "flex", justifyContent: "center", paddingRight: 70, paddingLeft: 70 }}></img>
+        </a>
+      </div>
+      <div className="land_img">
+        <a href="https://www.kw.ac.kr/ko/life/corona.jsp" target="_blank" rel="noopener noreferrer" >
+          <img src={p3} style={{ width: '100%', display: "flex", justifyContent: "center", paddingRight: 70, paddingLeft: 70 }}></img>
+        </a>
+      </div>
+    </Carousel>
 
-  )
+    <div className="table_user">
+    <Card title="Quick Menu">
+    <a href="https://klas.kw.ac.kr" target="_blank" rel="noopener noreferrer" >
+    <Card.Grid style={gridStyle}><span style={{color:'black'}}>KLAS</span></Card.Grid>
+    </a>
+    <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" >
+    <Card.Grid style={gridStyle}><YoutubeFilled style={{marginRight:3, color:'red'}}/><span style={{color:'black'}}>Youtube</span></Card.Grid>
+    </a>
+    <a href="https://github.com/rnin9/db_application_2020_KW" target="_blank" rel="noopener noreferrer" >
+   <Card.Grid style={gridStyle}><GithubFilled style={{marginRight:3, color:"black"}}/><span style={{color:'black'}}>Github</span></Card.Grid>
+    </a>
+    <a onClick={handleClick}>
+    <Card.Grid style={gridStyle}><TeamOutlined style={{marginRight:3}}/>Developers</Card.Grid>
+    </a>
+    </Card>
+    </div>
+  </div>
+
+)
 };
 
 export default LandingPage;
